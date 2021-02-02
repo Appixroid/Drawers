a0460f2288cee358fbd59e755fa9f554b1605869
Louis Parent <louisparent66@yahoo.fr>
2021-02-02 12:39:49 +0100

    Adding Fetch api support in ajax request

--- script/ajax.js
+++ script/ajax.js
@@ -1,3 +1,114 @@
+class XMLHttpRequestBuilder
+{
+	constructor(request) {
+		this.request = request;
+	}
+	
+	build(method, url, headers) {
+		this.request.open(method, url, true);
+		this.fillHeaders(headers);
+		return new BuildedXMLHttpRequest(this.request);
+	}
+	
+	fillHeaders(headers) {
+		if(headers !== undefined)
+		{
+			for(let header in headers)
+			{
+				this.request.setRequestHeader(header, headers[header]);
+			}
+		}
+	}
+};
+
+class BuildedXMLHttpRequest
+{
+	constructor(request) {
+		this.request = request;
+		this.AJAX_READY_STATE = 4;
+		this.HTTP_SUCCESS_CODE = 200;
+	}
+
+	send(data, onSuccess, onError) {
+		this.request.onreadystatechange = () => {
+			if(this.request.readyState === this.AJAX_READY_STATE)
+			{
+				if(this.request.status === this.HTTP_SUCCESS_CODE)
+				{
+					onSuccess(this.request.responseText);
+				}
+				else
+				{
+					onError(this.request.status);
+				}
+			}
+		};
+
+		this.request.send(data);
+	}
+};
+
+class FetchRequestBuilder
+{
+	constructor() {
+	}
+	
+	build(method, url, headers) {
+		var options = {
+			method: method,
+			headers: this.buildHeaders(headers),
+			mode: 'cors',
+			cache: 'default'
+		};
+	
+		return new BuildedFetchRequest(url, options);
+	}
+	
+	buildHeaders(headersData) {
+		let headers = new Headers();
+		
+		if(headers !== undefined)
+		{
+			for(let header in headersData)
+			{
+				headers.append(header, headersData[header]);
+			}
+		}
+		return headers;
+	}
+}; 
+
+class BuildedFetchRequest
+{
+	constructor(url, options)
+	{
+		this.url = url;
+		this.options = options;
+	}
+	
+	send(data, onSuccess, onError) {
+		if(data !== undefined)
+		{
+			this.options.body = data;
+		}
+		
+		fetch(this.url, this.options).then(response => {
+			if(response.ok)
+			{
+				response.text().then(result => {
+					onSuccess(result);			
+				});
+			}
+			else
+			{
+				onError("Network Error");
+			}
+		}).catch(error => {
+			onError(error);
+		});
+	}
+};
+
 const _AJAX_DRAWER_NAME = "ajax";
 const _Ajax = new DrawerBase(_AJAX_DRAWER_NAME);
 
@@ -8,85 +119,78 @@ _Ajax.URL_KEY_VALUE_SEPARATOR = "=";
 _Ajax.URL_ATTRIBUTE_SEPARATOR = "&";
 _Ajax.URL_QUERY_STRING_SEPARATOR = "?";
 
-_Ajax.AJAX_READY_STATE = 4;
-_Ajax.HTTP_SUCCESS_CODE = 200;
-
-_Ajax.getCorrectRequestObject = function() {
-	if (window.XMLHttpRequest)
-	{
-		return new XMLHttpRequest();
-	}
-	else
-	{
-		return new ActiveXObject("Microsoft.XMLHTTP");
-	} 
-};
-
-_Ajax.fillHeadersOfRequest = function(headers, request) {
-	if(headers !== undefined)
-	{
-		for(let header in headers)
-		{
-			request.setRequestHeader(header, headers[header]);
-		}
-	}
-};
-
 _Ajax.urlEncode = function(data) {
 	let url = "";
 	
-	for(let key in data)
+	if(data !== undefined)
 	{
-		url += key + this.URL_KEY_VALUE_SEPARATOR + data[key] + this.URL_ATTRIBUTE_SEPARATOR;
+		for(let key in data)
+		{
+			url += key + this.URL_KEY_VALUE_SEPARATOR + data[key] + this.URL_ATTRIBUTE_SEPARATOR;
+		}
 	}
 	
 	return url.endsWith(this.URL_ATTRIBUTE_SEPARATOR) ? url.substring(0, url.length - 1) : url;
 };
 
+_Ajax.toFormData = function(data) {
+	let formData = new FormData();
+	
+	if(data !== undefined)
+	{
+		for(let key in data)
+		{
+			formData.append(key, data[key]);
+		}
+	}
+	
+	return formData;
+};
+
+_Ajax.getRequestBuilder = function() {
+	if(window.fetch)
+	{
+		return new FetchRequestBuilder();
+	}	
+	else if (window.XMLHttpRequest)
+	{
+		return new XMLHttpRequestBuilder(new XMLHttpRequest());
+	}
+	else
+	{
+		return new XMLHttpRequestBuilder(new ActiveXObject("Microsoft.XMLHTTP"));
+	} 
+};
+
 _Ajax.request = function(method, url, postData, additionalHeaders, onSuccess, onError) {
-	let request = this.getCorrectRequestObject();
-	request.open(method, url, true);
-	this.fillHeadersOfRequest(additionalHeaders, request);
+	let requestBuilder = this.getRequestBuilder();
+	let request = requestBuilder.build(method, url, additionalHeaders);
 	
 	if(onSuccess === undefined && onError === undefined)
 	{
 		return new Promise((success, error) => {
-			this.sendRequest(request, postData, success, error);
+			request.send(postData, success, error);
 		});
 	}
 	else
 	{
-		this.sendRequest(request, postData, onSuccess, onError);
+		request.send(postData, success, error);
 	}
 };
 
-_Ajax.sendRequest = function(request, postData, onSuccess, onError) {
-	const AJAX_READY_STATE = this.AJAX_READY_STATE;
-	const HTTP_SUCCESS_CODE = this.HTTP_SUCCESS_CODE;
-	
-	request.onreadystatechange = function() {
-		if(this.readyState === AJAX_READY_STATE)
-		{
-			if(this.status === HTTP_SUCCESS_CODE)
-			{
-				onSuccess(this.responseText);
-			}
-			else
-			{
-				onError(this.status);
-			}
-		}
-	};
-
-	request.send(postData);
-};
-
 _Ajax.get = function(url, data, additionalHeaders) {
-	return this.request(this.GET_METHOD, url + this.URL_QUERY_STRING_SEPARATOR + this.urlEncode(data), "", additionalHeaders);
+	let fullUrl = url;
+	
+	if(data !== undefined)
+	{
+		fullUrl = url + this.URL_QUERY_STRING_SEPARATOR + this.urlEncode(data);
+	}
+	
+	return this.request(this.GET_METHOD, fullUrl, undefined, additionalHeaders);
 };
 
 _Ajax.post = function(url, data, additionalHeaders) {
-	return this.request(this.POST_METHOD, url, this.urlEncode(data), additionalHeaders);
+	return this.request(this.POST_METHOD, url, this.toFormData(data), additionalHeaders);
 };
 
 _Ajax.init = function() {};
